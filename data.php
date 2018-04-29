73eb13d021e0619053bd238735b2cb1eb63c53c0
heisenberg-2505 <2015csb1018@iitrpr.ac.in>
2018-04-29 17:52:56 +0530

    Initial Commit

---
+++ data.php
@@ -0,0 +1,18 @@
+// PHP script to create a database.
+<?php
+$servername = "localhost";
+$username = "project";
+$password = "csl301project";
+$conn = new mysqli($servername, $username, $password);
+if($conn->connection_error) {
+  echo("connection failed");
+}
+$sql = "CREATE DATABASE csl301";
+if($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+}
+else {
+  echo "Database creation error";
+}
+$conn->close();
+?>
