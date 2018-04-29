66283417243554152234823f26493176fe0757c9
heisenberg-2505 <2015csb1018@iitrpr.ac.in>
2018-04-29 20:17:56 +0530

    Added batch and department schema

---
+++ batchSchema.php
@@ -0,0 +1,21 @@
+<?php
+// php script to create the database schema table department.
+$servername = "localhost";
+$username = "project";
+$password = "csl301project";
+$dbname = "csl301";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connection_error) {
+  echo("connection failed: ". $conn->connect_error);
+}
+$sql = "CREATE TABLE Batch (
+  year INT PRIMARY KEY NOT NULL
+)";
+if($conn->query($sql) === TRUE) {
+  echo "Table created successfully";
+}
+else {
+  echo "Error creating table: " .$conn->error;
+}
+$conn->close();
+?>
---
+++ departmentSchema.php
@@ -0,0 +1,22 @@
+<?php
+// php script to create the database schema table department.
+$servername = "localhost";
+$username = "project";
+$password = "csl301project";
+$dbname = "csl301";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connection_error) {
+  echo("connection failed: ". $conn->connect_error);
+}
+$sql = "CREATE TABLE Department (
+  name VARCHAR(255) PRIMARY KEY NOT NULL,
+  building VARCHAR(255) NOT NULL
+)";
+if($conn->query($sql) === TRUE) {
+  echo "Table created successfully";
+}
+else {
+  echo "Error creating table: " .$conn->error;
+}
+$conn->close();
+?>
